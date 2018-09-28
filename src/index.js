af43eb041a57c45700fe775d082666a3162a4230
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-09-28 21:04:03 +0300

    refactor getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,50 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  const primes = (numbase) => {
-    const basePrime = [];
-    const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
-      37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
-      103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163,
-      167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
-      233, 239, 241, 251];
+  const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
+    37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
+    103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163,
+    167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+    233, 239, 241, 251];
 
-    for (let i = 53; i >= 0; i -= 1) {
-      if (numbase % primeNumbers[i] === 0) {
-        basePrime.push(primeNumbers[i]);
-      }
+  const basePrimes = primes.filter(prime => base % prime === 0);
+
+  const basePrimesCount = basePrimes.map((prime) => {
+    let power = 0;
+    let temp = base;
+
+    while (temp % prime === 0) {
+      power += 1;
+      temp = Math.floor(temp / prime);
     }
 
-    let powerCount;
-    let temp;
+    return [prime, power];
+  });
 
-    for (let j = 0; j < basePrime.length; j += 1) {
-      powerCount = 0;
-      temp = base;
+  const primesInFactorial = basePrimesCount.map((element) => {
+    const [prime, power] = element;
+    let counter = 0;
 
-      while (temp % basePrime[j] === 0) {
-        powerCount += 1;
-        temp = Math.floor(temp / basePrime[j]);
-      }
-
-      basePrime[j] = [basePrime[j], powerCount];
+    for (let i = 1; prime ** i <= number; i += 1) {
+      counter += Math.floor(number / (prime ** i));
     }
 
-    return basePrime;
-  };
+    return Math.floor(counter / power);
+  });
 
-  const baseNumArr = primes(base);
-  const countNumsArr = [];
-
-  for (let j = 0; j < baseNumArr.length; j += 1) {
-    const baseNum = baseNumArr[j][0];
-    let countNums = 0;
-
-    for (let i = 1; baseNum ** i <= number; i += 1) {
-      countNums += Math.floor(number / (baseNum ** i));
-    }
-    countNumsArr[j] = Math.floor(countNums / baseNumArr[j][1]);
-  }
-
-  countNumsArr.sort((a, b) => a - b);
-
-  return countNumsArr[0];
+  return Math.min(...primesInFactorial);
 };
